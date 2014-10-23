070e5e46ca429d50e41b7ce22779fa0ed9c0300c
Ilya Fateev <fat32nov@gmail.com>
2014-10-23 20:40:27 +0400

    Removing of dirs excluded from install.sh (Fuck all that shit)

--- install.sh
+++ install.sh
@@ -6,9 +6,5 @@ if [ ! -d ~/.vk-commits ]; then
     echo ~/.vk-commits directory created
 fi
 cd .vk-commits
-if [ -d ~/.vk-commits/dist ]; then
-    rm -rf ~/.vk-commits/dist
-fi
 git clone https://github.com/fat32nov/vk-commits.git dist
-echo vk-commits cloned
 ln -sv ~/.vk-commits/dist/vk-commits.py /usr/bin/vk-commits
