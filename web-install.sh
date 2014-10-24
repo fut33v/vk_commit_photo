9c56e919a90b0bd76f97589a101750425fb1a873
Ilya Fateev <fat32nov@gmail.com>
2014-10-24 18:05:39 +0400

    Fixing install.sh and web-install.sh (without sudo)

--- web-install.sh
+++ web-install.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+
 if [ ! -d ~/.vk-commits ]; then
-    mkdir -p ~/.vk-commits/dist
+    mkdir ~/.vk-commits
     echo ~/.vk-commits directory created
 fi
 
-cp -r *.py ~/.vk-commits/dist
+if [ -d ~/.vk-commits ]; then
+    rm -rf ~/.vk-commits/dist
+    echo ~/.vk-commits/dist directory removed
+fi
+
+cd ~/.vk-commits
+git clone https://github.com/fat32nov/vk-commits.git dist
 
 if [ -a /usr/bin/vk-commits ]; then
     echo link exists, replacing
